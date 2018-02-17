69c5b5e5e87f3f8733f87793ae834bffdcc00520
Wenish <jonas.voland@bluewin.ch>
2018-02-17 01:04:04 +0100

    update

--- src/rooms/battleroom/actionTypes.ts
+++ src/rooms/battleroom/actionTypes.ts
@@ -1,15 +1,34 @@
+//A
 export const AUTO_ATTACK_ENTITY = 'AUTO_ATTACK_ENTITY'
-
+//B
+//C
 export const CAST_ABILITY = 'CAST_ABILITY'
-
+//D
+//E
+//F
+//G
+//H
+//I
+export const INTERACT = 'INTERACT'
+//J
+//K
+//L
+//M
 export const MOVE_PLAYER_TO = 'MOVE_PLAYER_TO'
-
-export const PICK_UP_ITEM = 'PICK_UP_ITEM'
-
+//N
+//O
+//P
+//Q
+//R
+//S
 export const SEND_MESSAGE = 'SEND_MESSAGE'
-
 export const SEND_EMOTION = 'SEND_EMOTION'
-
 export const SEND_PING = 'SEND_PING'
-
+//T
+//U
 export const USE_ITEM = 'USE_ITEM'
+//V
+//W
+//X
+//Y
+//Z
